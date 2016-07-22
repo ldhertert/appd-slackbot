c285637d3e0adea0ce2f1c67980a11b43d0c8918
Luke Hertert <luke.hertert@gmail.com>
2016-07-22 09:19:47 -0700

    Logging and fix

--- index.js
+++ index.js
@@ -130,10 +130,12 @@ controller.hears([/response time for (.*) in (.*)/i], 'direct_mention,direct_mes
           }
       })
       .then(function (bt) {
+          console.log("BT: ", bt);
           return appDynamics.getMetricsForBT(message.match[2].replace("?", ""), bt.tierName, bt.name);
       })
       .then(function (metrics) {
-          bot.reply('The average response time for ' + message.match[1] + ' is ' + metrics.responseTime + "ms.");        
+          console.log("Metrics:", metrics);
+          return bot.reply(message, 'The average response time for ' + message.match[1] + ' is ' + metrics.responseTime + "ms.");        
       })
       .catch(function(err) {
         bot.reply(message, 'Sorry, something went wrong.'); 
